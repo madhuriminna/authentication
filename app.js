e0a5ec103bf5529ff8ffc533808abb04388c1186
madhuriminna <112750659+madhuriminna@users.noreply.github.com>
2022-09-19 18:40:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const dcrypt=require("dcrypt");
+const bcrypt=require("bcrypt");
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
